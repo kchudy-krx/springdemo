28b513f98948564ebca4eb2ae247a841ce7afe59
Krzysztof Chudy <krzysztof.chudy@noctuai.com>
2022-07-10 16:16:43 +0200

    tests passing

--- src/test/java/com/isa/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/isa/springdemo/SpringdemoApplicationTests.java
@@ -3,14 +3,11 @@ package com.isa.springdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 @SpringBootTest
 class SpringdemoApplicationTests {
 
     @Test
     void contextLoads() {
-        fail("nie działa!");
     }
 
 }
