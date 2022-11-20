1cb3cfe675e2b291508fdb624838ea8e45da2dbf
kchudy-krx <46656977+kchudy-krx@users.noreply.github.com>
2022-11-20 09:44:32 +0100

    Update SpringdemoApplication.java
    poprawka

--- src/main/java/com/isa/springdemo/SpringdemoApplication.java
+++ src/main/java/com/isa/springdemo/SpringdemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringdemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringdemoApplication.class, args)
+        SpringApplication.run(SpringdemoApplication.class, args);
     }
 
 }
